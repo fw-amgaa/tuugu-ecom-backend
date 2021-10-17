040295d81656facfaa47a5c0f0dc5f148be6eaee
amka <amka@mydoctors-Mac-mini.local>
2021-10-17 19:51:44 +0800

    added db connection with heroku

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE TABLE magazines (
+    id SERIAL PRIMARY KEY,
+    title varchar(255),
+    image varchar(255),
+    sub_title varchar(255),
+    author varchar(255),
+    published_date varchar(255),
+    description varchar(255),
+    body varchar(255)
+);
--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool
 
 const pool =  new Pool({
-    user: "amka",
-    password: "00231131",
-    database: "tuugu_db",
-    host: "localhost",
+    user: "yllirvpamiosdz",
+    password: "435603dcb9b12424ad51f1b286ddc01363f814177e8221d702482501d8ffb761",
+    database: "dsoced7jiio16",
+    host: "ec2-54-164-56-10.compute-1.amazonaws.com",
     port: 5432
 })
 
